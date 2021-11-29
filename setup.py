6ce032736a4a948cc9b5f9d02c65c45ed3454c7b
Jeff Wagner <mr.jeff.wagner@gmail.com>
2021-11-29 16:02:45 -0600

    initial commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()        
 
-setup(
+setuptools.setup(
     # Needed to silence warnings (and to be a worthwhile package)
     name='wagner',
     url='https://github.com/mrjeffwagner/sample_project',
