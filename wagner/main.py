9f1be197d616f03c0890821a2270aafeb085366c
Jeff Wagner <mr.jeff.wagner@gmail.com>
2021-11-30 08:59:20 -0600

    ran black formatter

--- wagner/main.py
+++ wagner/main.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
-def report(msg:str):
-    _time = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-6]
+
+def report(msg: str):
+    _time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f")[:-6]
     print(f"{_time}: {msg}")
